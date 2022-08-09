41b5828e0f717f23ad6f3c1bd2eef9f33eacdb0c
Dmitriy <zelentsov.dmitry@gmail.com>
2022-08-09 12:19:41 +0300

    add Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+int [] array = {1,2,3,4,5,6,7};
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number == array[5]||number == array[6])
+{
+   Console.WriteLine("Да,это выходной день");
+}
+else if(number < 1 || number > 7)
+{
+     Console.WriteLine("Не соответствует требованию");
+}
+else
+{
+     Console.WriteLine("Нет,это не выходной день");
+}
