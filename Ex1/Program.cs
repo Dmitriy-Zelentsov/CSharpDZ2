2e1cd240dee568669d76dfdcb592e3f5a284a54d
Dmitriy <zelentsov.dmitry@gmail.com>
2022-08-09 11:39:48 +0300

    add Ex1

---
+++ Ex1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+string str_number = number.ToString();
+char dig1 = str_number[1];
+Console.WriteLine(dig1);
